ed2edce55c8f22cff4fa478b7c33266fe32c3f9d
cyamao <yamao.cuzou@ynov.com>
2023-10-10 10:56:13 +0200

    [FIX] Number of country generated

--- src/generators/generateNationality.py
+++ src/generators/generateNationality.py
@@ -1,3 +1,10 @@
+##
+# @author Yamao Cuzou <yamao.cuzou@ynov.com>
+ # @file Description
+ # @desc Created on 2023-10-10 10:47:14 am
+ # @copyright Cuzou Corporation
+ #
+
 import sqlite3
 import random
 
@@ -5,7 +12,7 @@ import random
 #     nationalities = ["USA", "Canada", "UK", "Australia", "Germany", "France", "Japan", "China", "India", "Brazil", "Mexico"]
 #     return random.choice(nationalities)
 
-conn = sqlite3.connect('../../Database/company.db')
+conn = sqlite3.connect('../../Database/tests.db')
 cursor = conn.cursor()
 
 cursor.execute('''
@@ -15,8 +22,8 @@ cursor.execute('''
     );
 ''')
 
-nationalities = ["USA", "Canada", "UK", "Australia", "Germany", "France", "Japan", "China", "India", "Brazil", "Mexico"]
-for i in range(1, 11):
+nationalities = ["USA", "Canada", "UK", "Australia", "Germany", "France", "Japan", "China", "India", "Brazil", "Mexico", "Spanish", "Swiss", "Belgium", "Netherlands", "Finland", "Italia"]
+for i in range(1, 18):
     country = nationalities[i - 1]
     cursor.execute('INSERT INTO nationality (idNationality, Country) VALUES (?, ?);', (i, country))
 
