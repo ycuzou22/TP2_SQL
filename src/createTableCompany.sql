9ba79be70bd37625cd772bfc5607ebd00d454b03
cyamao <yamao.cuzou@ynov.com>
2023-10-10 09:56:51 +0200

    [FIX] MDP file and [ADD] New company hierarchie

--- src/createTableCompany.sql
+++ src/createTableCompany.sql
@@ -57,12 +57,95 @@ CREATE TABLE clients
     PRIMARY KEY(idClient)
 );
 
-CREATE TABLE company
+CREATE TABLE MarketingOfficers
 (
+    idMarketingOfficers INTEGER NOT NULL,
     idClient INTEGER NOT NULL,
     idClientType INTEGER NOT NULL,
     idHistorique INTEGER NOT NULL,
+    PRIMARY KEY(idMarketingOfficers),
     FOREIGN KEY(idHistorique) REFERENCES historiques(idHistorique),
     FOREIGN KEY(idClientType) REFERENCES clientTypes(idClientType),
     FOREIGN KEY(idClient) REFERENCES clients(idClient)
+);
+
+CREATE TABLE underMarketingDirection
+(
+    idUnderMarketingDirection INTEGER NOT NULL,
+    idMarketingOfficers INTEGER NOT NULL,
+    PRIMARY KEY(idUnderMarketingDirection),
+    FOREIGN KEY(idMarketingOfficers)
+    REFERENCES MarketingOfficers(idMarketingOfficers)
+);
+
+CREATE TABLE marketingDirection
+(
+    idMarketingDirection INTEGER NOT NULL,
+    idUnderMarketingDirection INTEGER NOT NULL,
+    PRIMARY KEY(idMarketingDirection),
+    FOREIGN KEY(idUnderMarketingDirection)
+    REFERENCES underMarketingDirection(idUnderMarketingDirection)
+);
+
+CREATE TABLE HRServiceOfficers
+(
+    idHRServiceOfficers INTEGER NOT NULL,
+    PRIMARY KEY(idHRServiceOfficers)
+);
+
+CREATE TABLE underHRDept
+(
+    idUnderHRDept INTEGER NOT NULL,
+    idHRServiceOfficers INTEGER NOT NULL,
+    PRIMARY KEY(idUnderHRDept),
+    FOREIGN KEY(idHRServiceOfficers)
+
+    REFERENCES HRServiceOfficers(idHRServiceOfficers)
+);
+
+CREATE TABLE HR
+(
+    idHR INTEGER NOT NULL,
+    idUnderHRDept INTEGER NOT NULL,
+    PRIMARY KEY(idHR),
+    FOREIGN KEY(idUnderHRDept)
+    REFERENCES underHRDept(idUnderHRDept)
+);
+
+CREATE TABLE ProductionOfficer
+(
+    idProductionOfficer INTEGER NOT NULL,
+    PRIMARY KEY(idProductionOfficer)
+);
+
+CREATE TABLE underProductionDirection
+(
+    idUnderProductionDirection INTEGER NOT NULL,
+    idProductionOfficer INTEGER NOT NULL,
+    PRIMARY KEY(idUnderProductionDirection),
+    FOREIGN KEY(idProductionOfficer)
+    REFERENCES ProductionOfficer(idProductionOfficer)
+);
+
+CREATE TABLE ProductionDirection
+(
+    idProductionDirection INTEGER NOT NULL,
+    idUnderProductionDirection INTEGER NOT NULL,
+    PRIMARY KEY (idProductionDirection),
+    FOREIGN KEY(idUnderProductionDirection)
+    REFERENCES underProductionDirection(idUnderProductionDirection)
+);
+
+CREATE TABLE generalDirection
+(
+    idGeneralDirection INTEGER NOT NULL,
+    idHR INTEGER NOT NULL,
+    idMarketingDirection INTEGER NOT NULL,
+    idProductionDirection INTEGER NOT NULL,
+    PRIMARY KEY (idGeneralDirection),
+    FOREIGN KEY(idHR) REFERENCES HR(idHR),
+    FOREIGN KEY(idMarketingDirection)
+    REFERENCES marketingDirection(idMarketingDirection),
+    FOREIGN KEY(idProductionDirection)
+    REFERENCES ProductionDirection(idProductionDirection)
 );
